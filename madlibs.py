1b1719db21db933ebc0e585f0ecb075ebc043911
Reika H <61173448+reikahayashi@users.noreply.github.com>
2021-01-23 18:38:21 -0500

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+#simple game of madlibs
+#input values and see what kind of story is created :)
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+animal = input("Favorite Animal: ")
+
+
+madlib = f"Today I plan on visiting the zoo and {verb1}! I can't wait to see the {animal} and {verb2} it. \
+It is going to be the {adj} day ever!"
+
+print(madlib)
